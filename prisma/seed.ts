3dfe7b5ca7699ae95b40e5005c6975557bd2671b
Paolo Flores <maximo_150@hotmail.com>
2024-10-01 20:36:23 -0500

    commit 2024-10-01 20:36

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import { categories } from "./data/categories";
+import { products } from "./data/products";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+async function main() {
+  try {
+    await prisma.category.createMany({
+      data: categories // Fuente de datos
+    })
+    await prisma.product.createMany({
+      data: products // Fuente de datos
+    })
+  }catch(error) {
+    console.log(error);
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.log(e);
+    await prisma.$disconnect()
+    process.exit(1)
+  })
